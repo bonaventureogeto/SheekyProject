812091400b4fb4088c66de8451697edd38f69574
kenprogrammer <kenprogrammer@gmail.com>
2020-09-24 22:13:40 +0300

    Imported python enviroment to enable reading .env file

--- sheeky_project/settings.py
+++ sheeky_project/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-# import environ
+import environ
 
 # env = environ.Env()
 # # reading .env file
-# environ.Env.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
