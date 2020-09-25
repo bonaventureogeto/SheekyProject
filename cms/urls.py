9954b9b5850d3f84322268851b276ccc883d9b62
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-09-25 10:15:42 +0300

    Created views for users to register accounts and login with their cridentials, added success and error message alerts

--- cms/urls.py
+++ cms/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path('service-categories', views.serviceCategories, name='service_categories'),
     path('service-subcategories', views.serviceSubCategories, name='service_subcategories'),
     path('service-details', views.serviceDetails, name='service_details'),
-    path('myaccount', views.myAccount, name='myaccount'),
     path('help', views.help, name='help'),
     path('booking-page', views.bookingPage, name='booking-page'),
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
+    path('myaccount', views.myAccount, name='myaccount'),
+    path('logout', views.logout, name='logout'),
 ]
--- cms/views.py
+++ cms/views.py
@@ -65,14 +65,76 @@ def serviceDetails(request):
     return render(request, 'frontend/service_details.html', context)
 
 
-# def service(request, service_id):
-#     service = get_object_or_404(Service, pk=service_id)
+#help
+def help(request):
+    return render(request, 'frontend/help.html')
 
-#     context = {
-#         'service': service
-#     }
 
-#     return render(request, 'frontend/service_details.html', context)
+#booking-page
+def bookingPage(request):
+    return render(request, 'frontend/booking_page.html')
+
+
+#register
+def register(request):
+    """
+    Register user
+    """
+    if request.method == 'POST':
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        phone_no = request.POST['phone_no']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            # check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'The username is already taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'The email is already registered')
+                    return redirect('register')
+                else:
+                    # looks good
+                    user = User.objects.create_user(
+                        username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(
+                        request, 'You are now registered. You can proceed to login')
+                    return redirect('login')
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'frontend/register.html')
+
+
+#login
+def login(request):
+    """ view for login """
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('booking_page')
+
+        else:
+            messages.error(request, 'Invalid email or password')
+            return redirect('login')
+    else:
+        return render(request, 'frontend/login.html')
 
 
 #myaccount
@@ -86,37 +148,6 @@ def myAccount(request):
     return render(request, 'frontend/myaccount.html', context)
 
 
-#help
-def help(request):
-    return render(request, 'frontend/help.html')
-
-
-#booking-page
-def bookingPage(request):
-    return render(request, 'frontend/booking_page.html')
-
-
 #register
-def register(request):
-    return render(request, 'frontend/register.html')
-
-
-#login
-def login(request):
-    """ view for login """
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        user = auth.authenticate(email=email, password=password)
-
-        if user is not None:
-            auth.login(request, user)
-            messages.success(request, 'You are now logged in')
-            return redirect('dashboard')
-
-        else:
-            messages.error(request, 'Invalid email or password')
-            return redirect('login')
-    else:
-        return render(request, 'frontend/login.html')
+def logout(request):
+    return redirect('index')
